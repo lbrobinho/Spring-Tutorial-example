642262e535dcf58b67957830e34d72617d9770b0
bo <m13163292006@gmail.com>
2018-09-03 17:54:24 -0400

    create springmvc-hibernate-project

--- spring-demo-annotations/src/com/bo/springdemo/FortuneService.java
+++
@@ -1,6 +0,0 @@
-package com.bo.springdemo;
-
-public interface FortuneService {
-
-    public String getDailyFortune();
-}
--- spring-demo-annotations/src/com/bo/springdemo/SadFortuneService.java
+++
@@ -1,9 +0,0 @@
-package com.bo.springdemo;
-
-public class SadFortuneService implements FortuneService{
-
-    @Override
-    public String getDailyFortune() {
-        return "Today is a sad day";
-    }
-}
