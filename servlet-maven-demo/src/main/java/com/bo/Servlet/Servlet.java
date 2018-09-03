b18ca0cbe9eb88832fbf72f18d1421510de3a631
Bo <lbrobinho@gwu.edu>
2018-09-03 15:25:35 -0400

    add servlet

---
+++ servlet-maven-demo/src/main/java/com/bo/Servlet/Servlet.java
@@ -0,0 +1,49 @@
+package com.bo.Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebServlet("/TestDbServlet")
+public class Servlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //set up connection variables
+        String user = "springstudent";
+
+        String pass = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?userSSL=false";
+
+        String driver = "com.mysql.jdbc.Driver";
+
+
+        //get connection to database
+
+        try {
+
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!!!");
+
+            myConn.close();
+        }
+
+        catch (Exception exc) {
+            exc.printStackTrace();
+            throw new ServletException(exc);
+        }
+    }
+}
