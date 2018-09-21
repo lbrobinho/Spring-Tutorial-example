2abf2e66d3437eddd6fa79dd3a4f8b376d45df0d
bo <m13163292006@gmail.com>
2018-09-21 14:19:38 -0400

    add custom login form

--- _07-spring-security-demo/src/main/java/com/bo/springsecurity/demo/config/DemoSecurityConfig.java
+++ _07-spring-security-demo/src/main/java/com/bo/springsecurity/demo/config/DemoSecurityConfig.java
@@ -3,6 +3,7 @@ package com.bo.springsecurity.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.User;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
         //add our users for in memory authentication
@@ -20,5 +22,16 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
                 .withUser(users.username("mary").password("test123").roles("MANAGER"))
                 .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl(("/authenticateTheUser"))
+                .permitAll();
     }
 }
